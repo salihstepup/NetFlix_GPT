97140763da26455a0ee90fa85404166bbaf16ff5
salihstepup <mohammedsalihred@gmail.com>
2025-12-29 22:52:39 +0530

    Signin Signup form

--- netflixgpt/src/components/Login.tsx
+++ netflixgpt/src/components/Login.tsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
 import Header from "./Header";
 //lex flex-col items-center justify-center min-h-screen bg-black text-white
 const Login = () => {
+  const [isSignInForm, setIsSignInForm] = useState(true);
+
+  const toggleSignInForm = () => {
+    setIsSignInForm(!isSignInForm);
+  };
+
   return (
     <div>
       <Header />
@@ -13,7 +20,32 @@ const Login = () => {
       </div>
 
       <form className="w-3/12 absolute p-12 bg-black/70 backdrop-blur-sm my-36 mx-auto right-0 left-0 text-white rounded-lg shadow-xl">
-        <h1 className="font-bold text-xl p-4">Sign In</h1>
+        {isSignInForm ? (
+          <h1 className="font-bold text-xl p-4">Sign In</h1>
+        ) : (
+          <h1 className="font-bold text-xl p-4">Sign Up</h1>
+        )}
+        {!isSignInForm && (
+          <input
+            type="text"
+            placeholder="First Name"
+            className="p-4 my-2 w-full bg-gray-700"
+          />
+        )}
+        {!isSignInForm && (
+          <input
+            type="text"
+            placeholder="Last Name"
+            className="p-4 my-2 w-full bg-gray-700"
+          />
+        )}
+        {/* {!isSignInForm && (
+          <input
+            type="number"
+            placeholder="Phone Number"
+            className="p-4 my-2 w-full bg-gray-700"
+          />
+        )} */}
         <input
           type="email"
           placeholder="Email or phone number"
@@ -24,9 +56,30 @@ const Login = () => {
           placeholder="Password"
           className="p-4 my-4 w-full bg-gray-700"
         />
-        <button type="submit" className="p-4 my-6 bg-red-500 w-full rounded-lg">
-          Sign In
-        </button>
+        {isSignInForm ? (
+          <button
+            type="submit"
+            className="p-4 my-6 bg-red-500 w-full rounded-lg"
+          >
+            Sign In
+          </button>
+        ) : (
+          <button
+            type="submit"
+            className="p-4 my-6 bg-red-500 w-full rounded-lg"
+          >
+            Sign Up
+          </button>
+        )}
+        {isSignInForm ? (
+          <p className="py-4" onClick={toggleSignInForm}>
+            New to Netflix? Signup Now
+          </p>
+        ) : (
+          <p className="py-4" onClick={toggleSignInForm}>
+            Already have an account? Sign In
+          </p>
+        )}
       </form>
     </div>
   );
